de006ea8ade817b24338cb8f59c4841e2859af0e
Shinjan saha <116014977+Shinjan-saha@users.noreply.github.com>
2023-08-03 20:48:28 +0530

    Update doublelinked.c

--- doublelinked.c
+++ doublelinked.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Node structure for doubly linked list
+
 struct Node {
     int data;
     struct Node* prev;
@@ -9,19 +9,19 @@ struct Node {
 };
 
 int main() {
-    // Creating nodes
+    
     struct Node* node1 = (struct Node*)malloc(sizeof(struct Node));
     struct Node* node2 = (struct Node*)malloc(sizeof(struct Node));
     struct Node* node3 = (struct Node*)malloc(sizeof(struct Node));
     struct Node* node4 = (struct Node*)malloc(sizeof(struct Node));
 
-    // Assigning data to nodes
+    
     node1->data = 11;
     node2->data = 22;
     node3->data = 33;
     node4->data = 44;
 
-    // Linking nodes together to form a doubly linked list
+    
     node1->next = node2;
     node2->prev = node1;
     node2->next = node3;
@@ -29,7 +29,7 @@ int main() {
     node3->next = node4;
     node4->prev = node3;
 
-    //  doubly linked list in forward
+    
     struct Node* current = node1;
     while (current != NULL) {
         printf("%d ", current->data);
